1d868d194646260d8cb6943658b8ddb8d082b933
Ahmed-Shafee <Ahmed_92a@hotmail.com>
2025-01-09 21:51:44 +0200

    update the data store in analysing process

--- src/conncet/server/analyse/file/ExcelWriter.java
+++ src/conncet/server/analyse/file/ExcelWriter.java
@@ -116,7 +116,6 @@ public class ExcelWriter {
     }
     
     
- 
     public static StringBuilder  positionsListForUser(String inputString) 
     {
         String serverUrl = "http://localhost:4000/process";
